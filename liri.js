f06347a7903b98114b9f58e3b8ebb7532f87ef7c
ShawnG <shawngway@gmail.com>
2019-09-23 18:20:26 -0400

    spotify

--- liri.js
+++ liri.js
@@ -12,7 +12,17 @@ switch (process.argv[2]) {
     case "spotify-this-song":
         // code block
         console.log("spotify-this-song working")
-
+        songName = process.argv[3];
+        spotify.search({ type: 'track', query: songName, limit: '1' }, function(err, data) {
+            if (err) {
+              return console.log('Error occurred: ' + err);
+            };
+           
+          console.log(data.tracks.items[0].album.artists[0].name);
+          console.log(songName);
+          console.log(data.tracks.items[0].album.external_urls);
+          console.log(data.tracks.items[0].album.name);
+          });
         break;
     case "movie-this":
         //code block
